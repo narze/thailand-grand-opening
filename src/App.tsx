504f910cda0f30db1814b57da21ff398720b782b
Manassarn Manoonchai <manassarn@gmail.com>
2021-06-16 23:26:20 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react'
+import dayjs from "dayjs"
+import relativeTime from 'dayjs/plugin/relativeTime'
+import 'dayjs/locale/th'
+
+import './App.css'
+
+dayjs.extend(relativeTime)
+dayjs.locale("th")
+
+function App() {
+  const [elapsed, setElapsed] = useState("...")
+  const [days, setDays] = useState("...")
+  const [hours, setHours] = useState("...")
+  const [minutes, setMinutes] = useState("...")
+  const [seconds, setSeconds] = useState("...")
+
+  const startTime = dayjs('2021-06-16 18:00')
+  const promisedTime = dayjs('2021-06-16 18:00').add(120, 'day')
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+    setElapsed(dayjs().from(startTime, true))
+    setDays(promisedTime.diff(dayjs(), "day").toLocaleString() + " วัน")
+    setHours((promisedTime.diff(dayjs(), "hour") % 24).toLocaleString() + " ชั่วโมง")
+    setMinutes((promisedTime.diff(dayjs(), "minute") % 60).toLocaleString() + " นาที")
+    setSeconds((promisedTime.diff(dayjs(), "second") % 60).toLocaleString() + " วินาที")
+    })
+
+    return () => { clearInterval(interval) }
+  }, [])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>“ผมตั้งเป้าเอาไว้ว่า ประเทศไทยจะต้องเปิดประเทศทั้งประเทศ ให้ได้ภายใน 120 วัน”</p>
+        <p id="by">ลุง - 16 มิ.ย. 2564 18:00 น.</p>
+        <p>
+          ตอนนี้ผ่านมาแล้ว {elapsed} ลุงเหลือเวลา...
+        </p>
+        <p id="remaining">
+          {days}
+        </p>
+        <p id="remaining-detail">
+          {hours} {minutes} {seconds}
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App

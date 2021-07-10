6e08a7e844beb437070662056d2e25f9cc724321
Manassarn Manoonchai <manassarn@gmail.com>
2021-07-11 03:58:11 +0700

    feat: Update links

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 
 const links = [
-  {
-    name: 'ร่วมช่วยกัน "ขอคนละชื่อรื้อระบอบประยุทธ์"',
-    url: "https://resolutionconformgen.web.app",
-  },
   { name: "Github", url: "https://github.com/narze/thailand-grand-opening" },
   { name: "@narze", url: "https://twitter.com/narze" },
   { name: "วาทะสลิ่มสุดเจ๋ง", url: "https://watasalim.vercel.app" },
+  { name: "ติด vs ตรวจ", url: "https://tid-vs-truad.vercel.app" },
 ];
 export default function Footer() {
   return (
